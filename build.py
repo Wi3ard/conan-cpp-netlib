c3642a6155b786c16434ba705958885beca8bb47
Alex Kucher <a.kucher@avantize.com>
2016-12-25 18:24:32 +0200

    Limit list of gcc versions

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
 from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(visual_versions = [14])
+    builder = ConanMultiPackager(visual_versions = [14], gcc_versions = ["4.8", "4.9", "5.2", "5.3"])
     builder.add_common_builds(shared_option_name="cpp-netlib:shared")
     builder.run()
